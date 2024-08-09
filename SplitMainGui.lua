d4676c228b3fac447d790a4e4980061c37d83118
AKNALOGIAa <macsss00711@gmail.com>
2024-08-10 01:31:12 +0300

    Test

--- SplitMainGui.lua
+++ SplitMainGui.lua
@@ -1483,7 +1483,7 @@ local function createTradeCard(trade)
                 itemLabel.TextColor3 = Color3.new(1, 1, 1)
     
                 -- Получаем значение и количество предмета
-                local itemValue = item:FindFirstChild("Value") and item.Value or "-"
+                local itemValue = item.Value or "-"
                 local itemCount = item:FindFirstChild("Count") and item.Count.Value or 1
                 itemLabel.Text = tostring(itemValue) .. ":" .. tostring(itemCount)
     
