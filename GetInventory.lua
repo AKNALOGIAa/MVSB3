7685bade2091dfa0169dff127bb1693f8448844b
AKNALOGIAa <116485847+AKNALOGIAa@users.noreply.github.com>
2024-08-16 22:32:12 +0300

    2.0 Update

--- GetInventory.lua
+++ GetInventory.lua
@@ -7,12 +7,22 @@ local categorizedItems = {}
 local HttpService = game:GetService("HttpService")
 local filename = playerName .. "_inventory.txt"
 
+if isfile("sb3AKNA//" .. filename) then
+    delfile("sb3AKNA//" .. filename)
+end
+
 if not isfolder("sb3AKNA") then
     makefolder("sb3AKNA")
 end
 
-if isfile("sb3AKNA//" .. filename) then
-    categorizedItems = HttpService:JSONDecode(readfile("sb3AKNA//" .. filename))
+local function getSizeValue(size)
+    local sizeMap = {50, 55, 60, 65, 70, 75, 80, 85, 90, 95, 100}
+    return sizeMap[size] or 50
+end
+
+local function getSpeedValue(speed)
+    local speedMap = {30, 31, 32, 33, 34, 35, 36, 37, 38, 39, 40}
+    return speedMap[speed] or 30
 end
 
 for _, item in ipairs(inventoryPath) do
@@ -24,17 +34,17 @@ for _, item in ipairs(inventoryPath) do
     if item:FindFirstChild("Size") or item:FindFirstChild("Speed") then
         itemCategory = "Mount"
         if item:FindFirstChild("Size") then
-            itemDetails = itemDetails .. " Size.Value = " .. item.Size.Value
+            itemDetails = itemDetails .. " " .. getSizeValue(item.Size.Value)
         end
         if item:FindFirstChild("Speed") then
-            itemDetails = itemDetails .. " Speed.Value = " .. item.Speed.Value
+            itemDetails = itemDetails .. " " .. getSpeedValue(item.Speed.Value)
         end
     end
 
     -- Категория Materials
     if item:FindFirstChild("Count") then
         itemCategory = "Materials"
-        itemDetails = itemDetails .. " Count.Value = " .. item.Count.Value
+        itemDetails = itemDetails .. " " .. item.Count.Value
     end
 
     -- Категория Aura
@@ -42,11 +52,11 @@ for _, item in ipairs(inventoryPath) do
         itemCategory = "Aura"
     end
 
-    -- Категория Экипировка
+    -- Категория Equipment
     if item:FindFirstChild("Upgrade") or item:FindFirstChild("Enchant") or item:FindFirstChild("LegendEnchant") then
-        itemCategory = "Экипировка"
+        itemCategory = "Equipment"
         if item:FindFirstChild("Upgrade") then
-            itemDetails = itemDetails .. " Upgrade.Value = " .. item.Upgrade.Value
+            itemDetails = itemDetails .. " Upgrade: " .. item.Upgrade.Value
         end
         if item:FindFirstChild("Enchant") then
             local enchantValue = item.Enchant.Value
@@ -74,7 +84,7 @@ for _, item in ipairs(inventoryPath) do
 end
 
 local function Save()
-    writefile("sb3AKNA//" .. filename, HttpService:JSONEncode(categorizedItems))
+    writefile("sb3AKNA//" .. filename, table.concat(categorizedItems, "\n"))
 end
 
 Save()
